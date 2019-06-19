d9aa001287df2d277856310e8ed7e92bcc02067e
Juan Garrido Peyres <jpeyresg@gmail.com>
2019-06-19 08:24:10 +0200

    Marcador funcionando

--- CrazyShark/Scene.swift
+++ CrazyShark/Scene.swift
@@ -12,47 +12,37 @@ import GameplayKit
 
 class Scene: SKScene {
     
-    let sharkScore = SKLabelNode()
-    let sharkScoreIcon = SKSpriteNode()
+    var sharkGame: SharkGameProtocol?
+
     var sharkScoreText = 0 {
         didSet{
-            self.sharkScore.text = "\(sharkScoreText)"
+            self.sharkGame?.sharkScoreDidChange(score: self.sharkScoreText)
         }
     }
-    let playerScore = SKLabelNode()
-    let playerScoreIcon = SKSpriteNode()
+
     var playerScoreText = 0 {
         didSet{
-            self.playerScore.text = "\(playerScoreText)"
+            self.sharkGame?.playerScoreDidChange(score: self.playerScoreText)
         }
     }
-//    var timer : Timer?
-    var sharkSprite = SKSpriteNode(imageNamed: "shark_first")
-    var targetsCreated = 0
-
+    var timer : Timer?
+    enum fishType {
+        case shark, firstFish, secondFish, thirdFish
+    }
+    var spritesCreated = 0
     
 //    let startTime = Date()
 //    let deathSound = SKAction.playSoundFileNamed("QuickDeath", waitForCompletion: false)
     
     override func didMove(to view: SKView) {
         // Setup your scene here
-        sharkScore.fontSize = 30
-        sharkScore.fontName = "Avenir Next"
-        sharkScore.color = .white
-        sharkScore.position = CGPoint(x: view.frame.maxX-150, y: view.frame.midY)
-        addChild(sharkScore)
-
         
-        playerScore.fontSize = 30
-        playerScore.fontName = "Avenir Next"
-        playerScore.color = .white
-        playerScore.position = CGPoint(x: view.frame.maxX-50, y: view.frame.maxY)
-        addChild(playerScore)
         
         sharkScoreText = 0
         playerScoreText = 0
-        
-        createShark()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (timer) in
+            self.createSprite()
+        })
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -64,25 +54,52 @@ class Scene: SKScene {
         //mirar si el toque cae dentro de nuestra vista de AR
         guard let touch = touches.first else {return}
         let location = touch.location(in: self)
+        
         //buscaremos todos los nodos que han sido tocados por ese toque de usuario
         let hit = nodes(at: location)
         //cogeremos el primer sprite del array que nos devuelve el método anterior (si lo hay) y animaremos ese pokemon hasta hacerlo desaparecer
         if let sprite = hit.first{
-            let scaleOut = SKAction.scale(to: 2, duration: 0.4)
-            let fadeOut = SKAction.fadeOut(withDuration: 0.4)
-            let remove = SKAction.removeFromParent()
-            let groupAction = SKAction.group([scaleOut, fadeOut])
-            let sequenceAction = SKAction.sequence([groupAction, remove])
+            guard let spriteName = sprite.name else{return}
             
-            sprite.run(sequenceAction)
-
+            switch spriteName {
+            case "shark":
+                return
+            case "firstFish":
+                playerScoreText += 10
+                killFish(sprite: sprite)
+            case "secondFish":
+                playerScoreText += 20
+                killFish(sprite: sprite)
+            case "thirdFish":
+                playerScoreText += 30
+                killFish(sprite: sprite)
+            default:
+                return
+            }
             
         }
         //actualizaremos que hay un pokemon menos con la variable targetCount
     }
     
-    func createShark(){
+    func killFish(sprite: SKNode){
+        let scaleOut = SKAction.scale(to: 2, duration: 0.4)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.4)
+        let remove = SKAction.removeFromParent()
+        let groupAction = SKAction.group([scaleOut, fadeOut])
+        let sequenceAction = SKAction.sequence([groupAction, remove])
+        sprite.run(sequenceAction)
+    }
+    
+    func createSprite(){
         
+        if spritesCreated == 46 {
+            timer?.invalidate()
+            timer = nil
+            return
+        }
+        
+        
+        // Switch que definirá el aranchor.name o nombre del ancla que vendrá de un struct
         guard let sceneView = self.view as? ARSKView else {return}
         
         // 1. Crear un generador de números aleatorios
@@ -95,15 +112,34 @@ class Scene: SKScene {
         let rotation = simd_mul(rotateX, rotateY)
         //5. Crear una translación de 1.5 metros en la dirección de la pantalla
         var translation = matrix_identity_float4x4
-        translation.columns.3.z = -1.5
+        translation.columns.3.z = -2
         //6. Combinar la rotación del paso 4 con la translación del paso 5
         let finalTransform = simd_mul(rotation, translation)
         //7. Crear un punto de ancla en el punto final determinado en el paso 6
-        let anchor = ARAnchor(transform: finalTransform)
-        addChild(sharkSprite)
-        //8. Añadir esa ancla a la escena
-
-        
+//        let anchor = ARAnchor(transform: finalTransform)
+        switch spritesCreated {
+        case 0:
+            let anchor = ARAnchor(name: "shark", transform: finalTransform)
+            sceneView.session.add(anchor: anchor)
+        case 1...31:
+            let anchor = ARAnchor(name: "firstFish", transform: finalTransform)
+            sceneView.session.add(anchor: anchor)
+        case 31...41:
+            let anchor = ARAnchor(name: "secondFish", transform: finalTransform)
+            sceneView.session.add(anchor: anchor)
+        case 41...46:
+            let anchor = ARAnchor(name: "thirdFish", transform: finalTransform)
+            sceneView.session.add(anchor: anchor)
+        default:
+            let anchor = ARAnchor(name: "firstFish", transform: finalTransform)
+            sceneView.session.add(anchor: anchor)
+        }
+        spritesCreated += 1
     }
     
+}
+
+protocol SharkGameProtocol {
+    func sharkScoreDidChange(score: Int)
+    func playerScoreDidChange(score: Int)
 }
--- CrazyShark/ViewController.swift
+++ CrazyShark/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 import SpriteKit
 import ARKit
 
-class ViewController: UIViewController, ARSKViewDelegate {
+class ViewController: UIViewController, ARSKViewDelegate, SharkGameProtocol {
+
+    
     
     @IBOutlet var sceneView: ARSKView!
+    var spriteCounter = 0
+    var sharkCounter = 0
+    var firstFishCounter = 0
+    var secondFishCounter = 0
+    var thirdFishCounter = 0
+    @IBOutlet var sharkScoreText: UILabel!
+    @IBOutlet var playerScoreText: UILabel!
+    var sharkScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
         sceneView.delegate = self
-        
+
         // Show statistics such as fps and node count
         sceneView.showsFPS = true
         sceneView.showsNodeCount = true
@@ -27,6 +37,11 @@ class ViewController: UIViewController, ARSKViewDelegate {
         // Load the SKScene from 'Scene.sks'
         if let scene = SKScene(fileNamed: "Scene") {
             sceneView.presentScene(scene)
+            if let sharkGameScene = scene as? Scene {
+                sharkGameScene.sharkGame = self
+            }
+//            let sharkGameScene = Scene()
+//            sharkGameScene.sharkGame = self
         }
     }
     
@@ -38,6 +53,7 @@ class ViewController: UIViewController, ARSKViewDelegate {
 
         // Run the view's session
         sceneView.session.run(configuration)
+        
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -51,10 +67,29 @@ class ViewController: UIViewController, ARSKViewDelegate {
     
     func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
         // Create and configure a node for the anchor added to the view's session.
-        let labelNode = SKLabelNode(text: "👾")
-        labelNode.horizontalAlignmentMode = .center
-        labelNode.verticalAlignmentMode = .center
-        return labelNode;
+        let fishType = anchor.name
+        switch fishType {
+        case "shark":
+            let sprite = SKSpriteNode(imageNamed: "sharkFirst")
+            sprite.name = "shark"
+            return sprite
+        case "firstFish":
+            let sprite = SKSpriteNode(imageNamed: "firstFish")
+            sprite.name = "firstFish"
+            return sprite
+        case "secondFish":
+            let sprite = SKSpriteNode(imageNamed: "secondFish")
+            sprite.name = "secondFish"
+            return sprite
+        case "thirdFish":
+            let sprite = SKSpriteNode(imageNamed: "thirdFish")
+            sprite.name = "thirdFish"
+            return sprite
+        default:
+            let sprite = SKSpriteNode(imageNamed: "firstFish")
+            sprite.name = "firstFish"
+            return sprite
+        }
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
@@ -71,4 +106,13 @@ class ViewController: UIViewController, ARSKViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    func sharkScoreDidChange(score: Int) {
+        self.sharkScoreText.text = "\(score)"
+    }
+    
+    func playerScoreDidChange(score: Int) {
+        self.playerScoreText.text = "\(score)"
+    }
+    
 }
